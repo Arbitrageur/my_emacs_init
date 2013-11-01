b2d8e978b3b7f4b13a5641eab30e3dbd1ccd3321
Yeolhyun Kwon <yeolhyun.kwon@gmail.com>
2013-11-01 10:10:44 +0900

    First commit

---
+++ init.el
@@ -0,0 +1,329 @@
+2;; emacs kicker --- kick start emacs setup
+;; Copyright (C) 2010 Dimitri Fontaine
+;;
+;; Author: Dimitri Fontaine <dim@tapoueh.org>
+;; URL: https://github.com/dimitri/emacs-kicker
+;; Created: 2011-04-15
+;; Keywords: emacs setup el-get kick-start starter-kit
+;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
+;;
+;; This file is NOT part of GNU Emacs.
+(setq max-lisp-eval-depth 9999)
+(setq max-specpdl-size 9999)
+
+;; Exec-path
+(setq exec-path (append '("/usr/local/bin") exec-path))
+(setq exec-path (append '("~/bin") exec-path))
+(setq exec-path (append '("/Library/Frameworks/Python.framework/Versions/2.7/bin/") exec-path))
+
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+(elpy-enable)
+
+(load-theme 'sanityinc-tomorrow-day t)
+
+(require 'cl)				; common lisp goodies, loop
+
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil t)
+  (url-retrieve
+   "https://github.com/dimitri/el-get/raw/master/el-get-install.el"
+   (lambda (s)
+     (end-of-buffer)
+     (eval-print-last-sexp))))
+
+;; now either el-get is `require'd already, or have been `load'ed by the
+;; el-get installer.
+;;(add-to-list 'load-path "~/.emacs.d/el-get/git-commit-mode")
+;; set local recipes
+(setq
+ el-get-sources
+ '((:name buffer-move			; have to add your own keys
+	  :after (progn
+		   (global-set-key (kbd "<C-S-up>")     'buf-move-up)
+		   (global-set-key (kbd "<C-S-down>")   'buf-move-down)
+		   (global-set-key (kbd "<C-S-left>")   'buf-move-left)
+		   (global-set-key (kbd "<C-S-right>")  'buf-move-right)))
+
+   (:name smex				; a better (ido like) M-x
+	  :after (progn
+		   (setq smex-save-file "~/.emacs.d/.smex-items")
+		   (global-set-key (kbd "M-x") 'smex)
+		   (global-set-key (kbd "M-X") 'smex-major-mode-commands)))
+
+   (:name google-translate
+	  :type git
+	  :url "https://github.com/manzyuk/google-translate"
+	  :compile "google-translate.el")
+
+   (:name emacs-flymake
+	  :type git
+	  :url "https://github.com/illusori/emacs-flymake"
+	  :compile "flymake.el")
+
+   ;; (:name magit				; git meet emacs, and a binding
+   ;; 	  :after (progn
+   ;; 		   (global-set-key (kbd "C-x C-z") 'magit-status)))
+
+   (:name goto-last-change		; move pointer back to last change
+	  :after (progn
+		   ;; when using AZERTY keyboard, consider C-x C-_
+		   (global-set-key (kbd "C-x C-/") 'goto-last-change)))))
+
+;; now set our own packages
+(setq
+ my:el-get-packages
+ '(el-get				; el-get is self-hosting
+   escreen            			; screen for emacs, C-\ C-h
+   color-theme-sanityinc
+   color-theme-tomorrow
+   color-theme-solarized
+   color-theme-tangotango
+;;   php-mode-improved			; if you're into php...
+   switch-window			; takes over C-x o
+   auto-complete			; complete as you type with overlays
+   auto-complete-emacs-lisp
+   auto-complete-yasnippet  ; yasnippet
+   auto-complete-clang
+   zencoding-mode			; http://www.emacswiki.org/emacs/ZenCoding
+;;   color-theme		                ; nice looking emacs
+   color-theme-tango
+   ace-jump-mode
+   expand-region
+   powerline
+   ))	                ; check out color-theme-solarized
+
+;;
+;; Some recipes require extra tools to be installed
+;;
+;; Note: el-get-install requires git, so we know we have at least that.
+;;
+;;(when (el-get-executable-find "cvs")
+;;  (add-to-list 'my:el-get-packages 'emacs-goodies-el)) ; the debian addons for emacs
+
+(when (el-get-executable-find "svn")
+  (loop for p in '(psvn    		; M-x svn-status
+		   yasnippet		; powerful snippet mode
+		   )
+	do (add-to-list 'my:el-get-packages p)))
+
+(setq my:el-get-packages
+      (append
+       my:el-get-packages
+       (loop for src in el-get-sources collect (el-get-source-name src))))
+
+;; install new packages and init already installed packages
+(el-get 'sync my:el-get-packages)
+
+(require 'powerline)
+(require 'flymake)
+(setq flymake-run-in-place nil)
+(setq temporary-file-directory "~/.emacs.d/tmp")
+
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/el-get/auto-complete/dict")
+
+;; auto-complete-clang
+(require 'auto-complete-clang)
+
+
+;;(setq ac-auto-start nil)
+;;(ac-set-trigger-key "TAB")
+;;(ac-set-trigger-key "M-TAB")
+;;(define-key ac-mode-map [(control tab)] 'auto-complete)
+(defun my-ac-config ()
+  (setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))
+  (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
+  ;; (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
+  (add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)
+  (add-hook 'css-mode-hook 'ac-css-mode-setup)
+  (add-hook 'auto-complete-mode-hook 'ac-common-setup)
+  (global-auto-complete-mode t))
+(defun my-ac-cc-mode-setup ()
+  (setq ac-sources (append '(ac-source-clang ac-source-yasnippet) ac-sources)))
+(add-hook 'c-mode-common-hook 'my-ac-cc-mode-setup)
+
+(require 'auto-complete-emacs-lisp)
+(add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-setup)
+;; ac-source-gtags
+(my-ac-config)
+
+;; yasnippet
+(require 'yasnippet)
+(yas-global-mode 1)
+(setq yas-snippet-dirs '("~/.emacs.d/el-get/yasnippet/snippets"))
+(mapc 'yas-load-directory yas-snippet-dirs)
+
+;; google-translate
+(require 'google-translate)
+(global-set-key "\C-ct" 'google-translate-at-point)
+
+(setq google-translate-enable-ido-completion 1)
+(setq google-translate-default-source-language nil)
+(setq google-translate-default-target-language nil)
+
+;; on to the visual settings
+(setq inhibit-splash-screen t)		; no splash screen, thanks
+(line-number-mode 1)			; have line numbers and
+(column-number-mode 1)			; column numbers in the mode line
+
+(tool-bar-mode -1)			; no tool bar with icons
+(scroll-bar-mode -1)			; no scroll bars
+(unless (string-match "apple-darwin" system-configuration)
+  ;; on mac, there's always a menu bar drown, don't have it empty
+  (menu-bar-mode -1))
+
+;; choose your own fonts, in a system dependant way
+(if (string-match "apple-darwin" system-configuration)
+    (set-face-font 'default "Monaco-12")
+  (set-face-font 'default "Monospace-10"))
+
+(global-hl-line-mode)			; highlight current line
+(global-linum-mode 1)			; add line numbers on the left
+(global-auto-complete-mode)		; auto-complete mode
+
+;; avoid compiz manager rendering bugs
+(add-to-list 'default-frame-alist '(alpha . 100))
+
+;; copy/paste with C-c and C-v and C-x, check out C-RET too
+(cua-mode)
+
+;; under mac, have Command as Meta and keep Option for localized input
+(when (string-match "apple-darwin" system-configuration)
+  (setq mac-allow-anti-aliasing t)
+;;  (setq mac-command-modifier 'meta)
+;;  (setq mac-option-modifier 'none)
+  ;; Encoding system
+  (set-terminal-coding-system 'utf-8)
+  (set-keyboard-coding-system 'utf-8)
+  (setq mac-control-modifier 'control)
+  (setq mac-option-modifier 'meta)
+  (setq mac-command-modifier 'super)
+  (setq ns-function-modifier 'hyper)
+  )
+
+;; Use the clipboard, pretty please, so that copy/paste "works"
+(setq x-select-enable-clipboard t)
+
+;; Navigate windows with M-<arrows>
+(windmove-default-keybindings 'meta)
+(setq windmove-wrap-around t)
+
+; winner-mode provides C-<left> to get back to previous window layout
+(winner-mode 1)
+
+;; whenever an external process changes a file underneath emacs, and there
+;; was no unsaved changes in the corresponding buffer, just revert its
+;; content to reflect what's on-disk.
+(global-auto-revert-mode 1)
+
+;; M-x shell is a nice shell interface to use, let's make it colorful.  If
+;; you need a terminal emulator rather than just a shell, consider M-x term
+;; instead.
+(autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+
+;; If you do use M-x term, you will notice there's line mode that acts like
+;; emacs buffers, and there's the default char mode that will send your
+;; input char-by-char, so that curses application see each of your key
+;; strokes.
+;;
+;; The default way to toggle between them is C-c C-j and C-c C-k, let's
+;; better use just one key to do the same.
+(require 'term)
+(define-key term-raw-map  (kbd "C-'") 'term-line-mode)
+(define-key term-mode-map (kbd "C-'") 'term-char-mode)
+
+;; Have C-y act as usual in term-mode, to avoid C-' C-y C-'
+;; Well the real default would be C-c C-j C-y C-c C-k.
+(define-key term-raw-map  (kbd "C-y") 'term-paste)
+
+;; use ido for minibuffer completion
+(require 'ido)
+(ido-mode t)
+(setq ido-save-directory-list-file "~/.emacs.d/.ido.last")
+(setq ido-enable-flex-matching t)
+(setq ido-use-filename-at-point 'guess)
+(setq ido-show-dot-for-dired t)
+
+;; default key to switch buffer is C-x b, but that's not easy enough
+;;
+;; when you do that, to kill emacs either close its frame from the window
+;; manager or do M-x kill-emacs.  Don't need a nice shortcut for a once a
+;; week (or day) action.
+(global-set-key (kbd "C-x C-b") 'ido-switch-buffer)
+(global-set-key (kbd "C-x C-c") 'ido-switch-buffer)
+(global-set-key (kbd "C-x B") 'ibuffer)
+
+;; C-x C-j opens dired with the cursor right on the file you're editing
+(require 'dired-x)
+
+;; full screen
+(defun fullscreen ()
+  (interactive)
+  (set-frame-parameter nil 'fullscreen
+		       (if (frame-parameter nil 'fullscreen) nil 'fullboth)))
+(global-set-key [f11] 'fullscreen)
+
+;; Default Windows Size
+(defun set-frame-size-according-to-resolution ()
+  (interactive)
+  (if window-system
+    (progn
+      (if (> (x-display-pixel-width) 1280)
+        (add-to-list 'default-frame-alist (cons 'width 200))
+        (add-to-list 'default-frame-alist (cons 'width 80)))
+      (add-to-list 'default-frame-alist
+                   (cons 'height (/ (- (x-display-pixel-height) 200)
+                                    (frame-char-height)))))))
+
+(set-frame-size-according-to-resolution)
+
+;; Org-mode
+(add-hook 'org-mode-hook (lambda ()  (setq org-agenda-include-diary t)))
+(setq org-default-notes-file "~/Documents/Orgs/notes.org")
+
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cl" 'org-store-link)
+
+;; Diary
+(setq diary-file "~/Dropbox/diary")
+(display-time)
+                    ;(add-hook 'diary-hook 'appt-make-list)
+(diary 0)
+
+(setq view-diary-entries-initially t
+      mark-diary-entries-in-calendar t
+      number-of-diary-entries 7)
+(add-hook 'diary-display-hook 'fancy-diary-display)
+(add-hook 'today-visible-calendar-hook 'calendar-mark-today)
+
+(defun diary-countdown (m1 d1 y1 n)
+  "Reminder during the previous n days to the date.
+    Order of parameters is M1, D1, Y1, N if
+    `european-calendar-style' is nil, and D1, M1, Y1, N otherwise."
+  (diary-remind '(diary-date m1 d1 y1) (let (value) (dotimes (number n value) (setq value (cons number value))))))
+
+;; Ace jump mode
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+
+(require 'expand-region)
+(global-set-key (kbd "H-+") 'er/expand-region)
+(global-set-key (kbd "H-_") 'er/contract-region)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default)))
+ '(org-agenda-files (quote ("~/Documents/Orgs/todo.org" "~/Documents/Orgs/work.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
